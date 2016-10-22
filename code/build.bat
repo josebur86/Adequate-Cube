b9c1b3767dec492295eed3e87c6c1be3215f89d8
Joe Burns <josebur86@gmail.com>
2016-10-21 19:08:36 -0500

    Update the build script

--- code/build.bat
+++ code/build.bat
@@ -3,6 +3,6 @@
 if not exist ..\build mkdir ..\build
 pushd ..\build
 
-cl /Od /Zi /nologo ..\code\win32_sidescroller.cpp /link user32.lib Gdi32.lib DSound.lib
+cl /Od /Zi /nologo ..\code\win32_aqcube.cpp /link user32.lib Gdi32.lib DSound.lib
 
 popd
