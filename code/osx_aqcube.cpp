9efcaadd6c40a3a665732133838f7c4a38e5f118
Joe Burns <josebur86@gmail.com>
2017-01-25 17:50:41 -0600

    Create and show a blank window that does nothing.

--- code/osx_aqcube.cpp
+++ code/osx_aqcube.cpp
@@ -1,7 +1,32 @@
+#include "SDL2/SDL.h"
+
 #include <stdio.h>
 
 int main(int argc, char** argv)
 {
-    printf("Hello Adequate Cube!\n");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("Unable to initialize SDL!\n");
+        return 1;
+    }
+
+    int NumberOfDisplays = SDL_GetNumVideoDisplays();
+    printf("Number of Displays: %i\n", NumberOfDisplays);
+
+    SDL_Window *Window = SDL_CreateWindow("Adequate Cube", 
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          800, 600,
+                                          0);
+    if (Window)
+    {
+        printf("Window created!\n");
+    }
+
+    while (1)
+    {
+    }
+
+    SDL_DestroyWindow(Window);
     return 0;
 }
