f3f2a74a14b14c4eb533d825ab8f338a95692fcc
Joe Burns <josebur86@gmail.com>
2017-01-27 15:39:22 -0600

    Fix gradient code and general cleanup.

--- code/osx_aqcube.cpp
+++ code/osx_aqcube.cpp
@@ -9,15 +9,15 @@ static void OSX_RenderGradient(void *Pixels, int Width, int Height)
 {
     assert(Pixels);
 
-    int *Pixel = (int *)Pixels;
+    uint *Pixel = (uint *)Pixels;
     for (int HeightIndex = 0; HeightIndex < Height; ++HeightIndex)
     {
         for (int WidthIndex = 0; WidthIndex < Width; ++WidthIndex)
         {
-            int8 R = (int8)WidthIndex;
-            int8 G = (int8)HeightIndex;
-            int8 B = (int8)WidthIndex+HeightIndex;
-            int Color = (R << 16) | (G << 8) | (B << 0);
+            uint8 R = (uint8)WidthIndex;
+            uint8 G = (uint8)HeightIndex;
+            uint8 B = (uint8)WidthIndex + 256 + (uint)HeightIndex + 128;
+            uint Color = (R << 16) | (G << 8) | (B << 0);
 
             *Pixel++ = Color;
         }
@@ -32,9 +32,6 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    int NumberOfDisplays = SDL_GetNumVideoDisplays();
-    printf("Number of Displays: %i\n", NumberOfDisplays);
-
     SDL_Window *Window = SDL_CreateWindow("Adequate Cube", 
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
@@ -42,16 +39,11 @@ int main(int argc, char** argv)
                                           0);
     if (Window)
     {
-        printf("Window created!\n");
-
-        // TODO(joe): Allocate video memory.
         SDL_Surface *Surface = SDL_GetWindowSurface(Window);
         if (Surface)
         {
-            SDL_PixelFormat *Format = Surface->format;
-            printf("W: %i H: %i BPP: %i\n", Surface->w, Surface->h, Format->BitsPerPixel);
-
             // TODO(joe): Allocate game memory.
+            // TODO(joe): Lock that update loop to 30 fps.
             // TODO(joe): Compile the game code into it's on .so file.
 
             bool GlobalRunning = true;
@@ -71,8 +63,16 @@ int main(int argc, char** argv)
                 SDL_UpdateWindowSurface(Window);
             }
         }
+        else
+        {
+            printf("Unable to get the window surface.");
+        }
 
         SDL_DestroyWindow(Window);
+    }
+    else
+    {
+        printf("Unable to create window.");
     }
 
     return 0;
