116d0e85eafde4329195ee6ed3861b91e6760088
Joe Burns <josebur86@gmail.com>
2017-01-30 23:01:36 -0600

    Nicer build script.

--- code/build.sh
+++ code/build.sh
@@ -7,11 +7,12 @@ echo "Building..."
 if [ ! -d ../build ]; then
     mkdir ../build
 fi
-pushd ../build
+pushd ../build > /dev/null
 
 CompilerFlags="-std=c++11 -g -O0"
 
 clang++ $CompilerFlags -shared -fpic ../code/aqcube.cpp -o aqcube.so
 clang++ $CompilerFlags ../code/osx_aqcube.cpp -L../code/libs -lSDL2 -o osx_aqcube
 
-popd
+popd > /dev/null
+echo "...Done!"
